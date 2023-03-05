d1ff3780dec5831e77f0b567091c01b6043e52cb
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2023-03-05 23:27:42 +0200

    completed architecture

--- client/src/app.js
+++ client/src/app.js
@@ -1,2 +1,18 @@
 // routing only and onlyyyyy routing, nothing else
+import page from '../node_modules/page/page.mjs';
+import { catalogue } from './views/movieCatalogue';
+import { loginPage } from './views/loginView.js';
+import { registerPage } from './views/registerView.js';
+import { setUpMiddleware } from './middlewares/setUpMidware.js';
+import { detailsPage } from './views/detailsView.js';
+import { addMoviePage } from './views/addMovieView.js';
+import { editMoviePage } from './views/editMovieView.js';
 
+page('/', setUpMiddleware, catalogue);
+page('/login', setUpMiddleware, loginPage);
+page('/register', setUpMiddleware, registerPage);
+page('/details/:id', setUpMiddleware, detailsPage);
+page('/add-movie', setUpMiddleware, addMoviePage);
+page('/edit-movie/:id', setUpMiddleware, editMoviePage);
+
+page.start();
--- client/src/middlewares/contextMiddleware.js
+++ client/src/middlewares/contextMiddleware.js
@@ -1,2 +1,10 @@
 // the idea is -> за всяка страница имаме едно и също поведение. Тоест във логин искаме да вземем нещо от
 // локален сторидж, в регистрацията също ни трябва, трябва ни НАВСЯКЪДЕ
+import { render } from '../../node_modules/lit-html/lit-html.js';
+
+const mainRootElement = document.getElementById('container');
+
+export function setUpMiddleware(ctx, next) {
+    ctx.render = (content) => render(content, mainRootElement);
+    next();
+}
